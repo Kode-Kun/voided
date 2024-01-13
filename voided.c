19bd6ce076763d0e03ffa8c3a806b180b7ac00e7
Kode-Kun <luisantcard@gmail.com>
2024-01-13 11:36:19 -0500

    removed binary and added raw mode to voided.c

--- voided.c
+++ voided.c
@@ -1,11 +1,36 @@
-#include <string.h>
-#include <stdint.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <termios.h>
 
-int main(int argc, char **argv)
-{
+struct termios orig_term;
 
+void disable_raw_mode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_term);
+}
+
+void enable_raw_mode(){
+    tcgetattr(STDIN_FILENO, &orig_term);
+    atexit(disable_raw_mode);
+
+    struct termios raw = orig_term;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main(){
+    enable_raw_mode();
+    char c;
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if(iscntrl(c)){
+            printf("%d\r\n", c);
+        } else{
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
     return 0;
 }
