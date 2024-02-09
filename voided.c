e0218caa8fc7d20797132462cbe571119f6c484b
Kode-Kun <luisantcard@gmail.com>
2024-02-09 14:43:14 -0500

    fixed bug from previous commit

--- voided.c
+++ voided.c
@@ -138,7 +138,6 @@ void voided_open(char *filename){
   char *line = NULL;
   size_t linecap = 0;
   ssize_t linelen;
-  linelen = getline(&line, &linecap, fp);
   while((linelen = getline(&line, &linecap, fp)) != -1){
     while(linelen > 0 && (line[linelen - 1] == '\n' ||
                           line[linelen - 1] == '\r'))
